f617ff39a9295a1671bd3acbe60c9dd5abad7a0c
EightBot <michael.stonis@gmail.com>
2014-10-26 12:55:35 -0500

    android animations

---
+++ EightBot.FluentAnimator.TestApp/MainActivity.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Animation;
+using Android.Views.Animations;
+
+namespace EightBot.FluentAnimator.TestApp
+{
+	[Activity (Label = "EightBot.FluentAnimator.TestApp", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			var fadeIn = FindViewById<Button> (Resource.Id.fadeIn);
+			var fadeOut = FindViewById<Button> (Resource.Id.fadeOut);
+
+			var wiggle = FindViewById<Button> (Resource.Id.wiggle);
+
+			var flip = FindViewById<ToggleButton> (Resource.Id.flip);
+
+			var spin = FindViewById<ToggleButton> (Resource.Id.spin);
+
+			var pulsate = FindViewById<ToggleButton> (Resource.Id.pulsate);
+
+			var image = FindViewById<ImageView> (Resource.Id.androidy);
+			
+			fadeIn.Click += delegate {
+				image.FadeIn();
+			};
+
+			fadeOut.Click += delegate {
+				image.FadeOut();
+			};
+
+			wiggle.Click += delegate {
+				image.Wiggle(10);
+			};
+
+			flip.CheckedChange += (s,e) => {
+				if(e.IsChecked)
+					image.Flip(EightBot.FluentAnimator.FluentPropertyAnimation.FlipDirection.LeftToRight, interpolator: new AnticipateInterpolator());
+				else
+					image.FlipReturn(EightBot.FluentAnimator.FluentPropertyAnimation.FlipDirection.LeftToRight, interpolator: new AnticipateInterpolator());
+			};
+
+			ValueAnimator spinAnimation = null;
+			spin.CheckedChange += (sender, e) => {
+				if(e.IsChecked)
+					spinAnimation = image.Spin(FluentPropertyAnimation.DefaultDuration * 2, interpolator: new AnticipateOvershootInterpolator());
+				else
+					spinAnimation.End();
+			};
+
+
+			ValueAnimator pulsateAnimation = null;
+			pulsate.CheckedChange += (sender, e) => {
+				if(e.IsChecked)
+					pulsateAnimation = image.Pulsate(1.5f);
+				else
+					pulsateAnimation.End();
+			};
+		}
+	}
+}
+
+
---
+++ EightBot.FluentAnimator/FluentPropertyAnimation.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Android.Views.Animations;
+using Android.Views;
+using Android.Animation;
+
+namespace EightBot.FluentAnimator
+{
+	public static class FluentPropertyAnimation
+	{
+
+		public const long DefaultDuration = 1200;
+
+		public static ValueAnimator FadeOut(this View view, long duration = DefaultDuration){
+
+			var animation = ObjectAnimator.OfFloat (view, "alpha", 1f, 0f);
+			animation.SetDuration (duration);
+
+			if(view.Alpha != 0f)
+				animation.Start ();
+
+			return animation;
+		}
+
+		public static ValueAnimator FadeIn(this View view, long duration = DefaultDuration){
+			var animation = ObjectAnimator.OfFloat (view, "alpha", 0f, 1f);
+			animation.SetDuration (duration);
+
+			if(view.Alpha != 1f)
+				animation.Start ();
+
+			return animation;
+		}
+
+		public static ValueAnimator Spin(this View view, long duration = DefaultDuration, ITimeInterpolator interpolator = null){
+			var rotation = ObjectAnimator.OfFloat (view, "rotation", 0, 360);
+			rotation.SetDuration (duration);
+			rotation.SetInterpolator (interpolator ?? new LinearInterpolator());
+			rotation.RepeatCount = ValueAnimator.Infinite;
+
+			rotation.Start ();
+
+			return rotation;
+		}
+
+		public enum FlipDirection
+		{
+			RightToLeft,
+			LeftToRight,
+			TopToBottom,
+			BottomToTop
+		}
+
+		public static ValueAnimator Flip(this View view, FlipDirection flipDirection, long duration = DefaultDuration, ITimeInterpolator interpolator = null){
+
+			var flipProperty = "rotationY";
+			var flipDegrees = -180f;
+
+			switch (flipDirection) {
+			case FlipDirection.LeftToRight:
+				flipDegrees = -flipDegrees;
+				break;
+			case FlipDirection.RightToLeft:
+				break;
+			case FlipDirection.TopToBottom:
+				flipProperty = "rotationX";
+				break;
+			case FlipDirection.BottomToTop:
+				flipProperty = "rotationX";
+				flipDegrees = -flipDegrees;
+				break;
+			}
+
+			var rotation = ObjectAnimator.OfFloat (view, flipProperty, 0, flipDegrees);
+			rotation.SetDuration (duration);
+			rotation.SetInterpolator (interpolator ?? new LinearInterpolator());
+
+			rotation.Start ();
+
+			return rotation;
+		}
+
+		public static ValueAnimator FlipReturn(this View view, FlipDirection flipDirection, long duration = DefaultDuration, ITimeInterpolator interpolator = null){
+		
+			var flipProperty = "rotationY";
+			var flipDegrees = -180f;
+
+			switch (flipDirection) {
+			case FlipDirection.LeftToRight:
+				flipDegrees = -flipDegrees;
+				break;
+			case FlipDirection.RightToLeft:
+				break;
+			case FlipDirection.TopToBottom:
+				flipProperty = "rotationX";
+				break;
+			case FlipDirection.BottomToTop:
+				flipProperty = "rotationX";
+				flipDegrees = -flipDegrees;
+				break;
+			}
+
+			var rotation = ObjectAnimator.OfFloat (view, flipProperty, flipDegrees, 0);
+
+			rotation.SetDuration (duration);
+			rotation.SetInterpolator (interpolator ?? new LinearInterpolator());
+
+			rotation.Start ();
+
+			return rotation;
+		}
+
+		public static ValueAnimator Pulsate(this View view, float pulsateSize = .9f, long duration = DefaultDuration){
+			var scale = ObjectAnimator.OfPropertyValuesHolder (
+				view,
+				PropertyValuesHolder.OfFloat ("scaleX", 1, pulsateSize),
+				PropertyValuesHolder.OfFloat ("scaleY", 1, pulsateSize)
+			);
+			scale.RepeatMode = ValueAnimatorRepeatMode.Reverse;
+			scale.SetDuration (duration / 2);
+			scale.RepeatCount = ValueAnimator.Infinite;
+			scale.Start ();
+			return scale;
+		}
+
+		public static ValueAnimator Wiggle(this View view, float wiggleAmount = .1f, long duration = DefaultDuration){
+
+			var viewWiggleAmount = wiggleAmount < 1f ? wiggleAmount * view.Width : wiggleAmount;
+
+			var scale = ObjectAnimator.OfPropertyValuesHolder (
+				view,
+				//PropertyValuesHolder.OfFloat ("scaleX", 1, pulsateSize),
+				PropertyValuesHolder.OfFloat ("translationX", viewWiggleAmount, -viewWiggleAmount)
+			);
+			scale.RepeatMode = ValueAnimatorRepeatMode.Reverse;
+			scale.SetDuration (duration / 2);
+			scale.RepeatCount = ValueAnimator.Infinite;
+			scale.Start ();
+			return scale;
+		}
+	}
+}
+
